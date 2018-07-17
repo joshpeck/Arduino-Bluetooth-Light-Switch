2228f0a365e10d911222c72a274211b52b93a243
joshpeck <coolalexxela@gmail.com>
2018-07-16 18:02:20 -0600

    Add files via upload

---
+++ BluetoothLightSwitch.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(13, OUTPUT);
+
+  servo.attach(13);
+  servo.write(42);
+  servo.detach();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, LOW);
+
+  if(Serial.available()>0){
+    char data = Serial.read();
+    switch(data){
+      case '0': servo.attach(13);servo.write(74);delay (1500);servo.write(42);delay (500);digitalWrite(13, LOW);servo.detach();break;
+      case '1': servo.attach(13);servo.write(13);delay (1500);servo.write(42);delay (500);digitalWrite(13, LOW);servo.detach();break;
+      default: break;
+    }
+    Serial.println(data);
+  }
+  delay(50);
+}
